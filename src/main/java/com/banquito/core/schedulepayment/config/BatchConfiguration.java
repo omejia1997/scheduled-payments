c10d1853447984f4f6c71b0d3f05dd8cefe83af3
Omar Willyams Mejia Caizaluisa <omardevaluado08@gmail.com>
2022-07-11 15:38:44 -0500

    [ADD] Configuration of Scheduled Task

--- src/main/java/com/banquito/core/schedulepayment/config/BatchConfiguration.java
+++ src/main/java/com/banquito/core/schedulepayment/config/BatchConfiguration.java
@@ -1,56 +1,58 @@
 package com.banquito.core.schedulepayment.config;
 
+import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.banquito.core.schedulepayment.task.LinesProcessor;
+import com.banquito.core.schedulepayment.task.LinesReader;
+import com.banquito.core.schedulepayment.task.LinesWriter;
 
 @Configuration
 @EnableBatchProcessing
 public class BatchConfiguration {
   @Autowired 
-    private JobBuilderFactory jobs;
+  private JobBuilderFactory jobs;
 
-    @Autowired 
-    private StepBuilderFactory steps;
+  @Autowired 
+  private StepBuilderFactory steps;
 
-    @Bean
-    protected Step readLines() {
-        return steps
-          .get("readLines")
-          .tasklet(linesReader())
-          .build();
-    }
+  @Bean
+  protected Step readLines() {
+      return steps
+        .get("readLines")
+        .tasklet(new LinesReader())
+        .build();
+  }
 
-    @Bean
-    protected Step processLines() {
-        return steps
-          .get("processLines")
-          .tasklet(linesProcessor())
-          .build();
-    }
+  @Bean
+  protected Step processLines() {
+      return steps
+        .get("processLines")
+        .tasklet(new LinesProcessor())
+        .build();
+  }
 
-    @Bean
-    protected Step writeLines() {
-        return steps
-          .get("writeLines")
-          .tasklet(linesWriter())
-          .build();
-    }
+  @Bean
+  protected Step writeLines() {
+      return steps
+        .get("writeLines")
+        .tasklet(new LinesWriter())
+        .build();
+  }
 
-    @Bean
-    public Job job() {
-        return jobs
-          .get("taskletsJob")
-          .start(readLines())
-          .next(processLines())
-          .next(writeLines())
-          .build();
-    }
-
+  @Bean
+  public Job job() {
+      return jobs
+        .get("taskletsJob")
+        .start(readLines())
+        .next(processLines())
+        .next(writeLines())
+        .build();
+  }
 }
--- src/main/java/com/banquito/core/schedulepayment/task/ScheduledTask.java
+++ src/main/java/com/banquito/core/schedulepayment/task/ScheduledTask.java
@@ -1,17 +1,25 @@
 package com.banquito.core.schedulepayment.task;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ScheduledTask {
 
-	@Scheduled(fixedRate = 5000)
-	public void reportCurrentTime() {
+	@Autowired
+	private JobLauncher jobLauncher;
+
+	@Autowired
+	private Job job;
+
+	@Scheduled(cron = "0 0 19 * * *")
+	public void executeJob() throws Exception {
+		JobParameters jobParameter = new JobParametersBuilder().addString("JobID",String.valueOf(System.currentTimeMillis())).toJobParameters();
+		jobLauncher.run(job,jobParameter);
 	}
 }
